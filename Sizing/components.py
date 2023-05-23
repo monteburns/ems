0f1c2d8b364218c3e85ab933af196e2c534b4c7c
Osman Yücehan Kutlu <OsmanYucehan.Kutlu@tenmak.gov.tr>
2023-05-23 16:46:42 +0300

    hydrogen unit added

--- Sizing/components.py
+++ Sizing/components.py
@@ -13,3 +13,17 @@ class Battery():
     def __int__(self, capacity):
         self.capacity = capacity
 
+class Hydrogen():
+
+    def __init__(self, capacity, eff):
+        self.storageCap = capacity
+        self.eff = eff
+
+    def mdot(self, Pe):
+        """The model assumes energy conversion based on a solid 
+    oxide electrolysis cell (SOEC). Using the higher heating value 
+    (HHV) of hydrogen, calculated hydrogen production rate is returned"""
+        HHV = 141.80e3 # kJ/kg
+        
+        return (self.eff * Pe)/HHV
+    
